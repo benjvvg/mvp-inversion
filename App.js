2f108c49e9db2567642c8cf87b6640fce9f89d69
Benjamiiinn <bi.gonzalezg@duocuc.cl>
2025-12-29 00:30:32 -0300

    Ruteo de la app

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import HomeScreen from './src/screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName='Home'
+          screenOptions={{
+            headerShown: false,
+            contentStyle: { backgroundColor: '#f3f4f6' }
+          }}
+        >
+          <Stack.Screen name='Home'  component={HomeScreen}/>
+        </Stack.Navigator>
+        <StatusBar style='auto' />
+      </NavigationContainer>
+    </SafeAreaProvider>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
